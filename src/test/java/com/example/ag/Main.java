d956ebe520abd8ddca21885b56099ffa08ad0a5b
Cleiton Ferreira <cleitonferreiraa@hotmail.com>
2022-02-18 12:44:29 -0300

    add o tempo de execução

--- src/test/java/com/example/ag/Main.java
+++ src/test/java/com/example/ag/Main.java
@@ -1,14 +1,11 @@
 package com.example.ag;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    private static AlgoritmoGenetico ag;
+    private static AlgoritmoGenetico algoritmoGenetico;
     
     /*
     *   PARÂMETROS DO ALGORITMO GENÉTICO
@@ -23,14 +20,73 @@ public class Main {
     // CONDIÇÃO DE PARADA: NÚMERO MÁXIMO DE GERAÇÕES SEM ATUALIZAR
     private static final int NUM_GERACOES = 1000;
     
-    private static final String NOME_ARQUIVO = "Teste_01.dat";
+    //private static final String NOME_ARQUIVO = "Teste_01.dat";
     
     public static void main(String[] args) throws IOException {
-        lerArquivo(NOME_ARQUIVO);
-        ag.executar();
+
+        lerArquivo("instancias/Teste_01.dat");
+        //lerArquivos("instancias/");
+
+    }
+
+    public static void lerArquivos(String nomeDiretorio) throws FileNotFoundException, IOException{
+
+        File file= new File(nomeDiretorio);
+        File[] listaArquivos = file.listFiles();
+        if(listaArquivos!=null){
+            for(File arquivo:listaArquivos) {
+                if (!arquivo.isDirectory() && arquivo.getAbsolutePath().endsWith(".dat")) {
+                    long start = System.nanoTime(); //tempo inicial
+                    String ww = arquivo.getAbsolutePath() + ": " + arquivo.getName();
+                    System.out.println(ww);
+
+                    FileInputStream stream = new FileInputStream(arquivo.getName());
+                    InputStreamReader reader = new InputStreamReader(stream);
+                    BufferedReader br = new BufferedReader(reader);
+                    String linha = br.readLine();
+
+                    //num linhas
+                    String strNumLinha = linha.substring(linha.indexOf(" ")).trim();
+                    int numLinha = Integer.parseInt(strNumLinha);
+
+                    linha = br.readLine();
+
+                    //num coluna
+                    String strNumColuna = linha.substring(linha.indexOf(" ")).trim();
+                    int numColuna = Integer.parseInt(strNumColuna);
+
+                    algoritmoGenetico = new AlgoritmoGenetico(numLinha, numColuna, TAM_POPULACAO, TAXA_MIN_MUTACAO, NUM_GERACOES);
+
+                    br.readLine();
+
+                    for (int i = 0; i < numColuna; i++) {
+                        linha = br.readLine();
+                        String dados[] = linha.split("\\s+");
+
+                        double custo = Double.parseDouble(dados[2]);
+                        algoritmoGenetico.addCusto(i, custo);
+
+                        for (int j = 3; j < dados.length; j++) {
+                            algoritmoGenetico.addDados(i, Integer.parseInt(dados[j]));
+                        }
+                    }
+
+                    algoritmoGenetico.executar();
+
+                    long finish = System.nanoTime(); //tempo final
+                    long time = (finish - start);
+                    long time_ms = TimeUnit.NANOSECONDS.toMillis(time);
+
+                    System.out.println("\nTempo de execução (ms): "+ time_ms);
+
+                }
+            }
+        }
+
     }
     
     public static void lerArquivo(String nomeArquivo) throws FileNotFoundException, IOException{
+        long start = System.nanoTime(); //tempo inicial
         FileInputStream stream = new FileInputStream(nomeArquivo);
         InputStreamReader reader = new InputStreamReader(stream);
         BufferedReader br = new BufferedReader(reader);
@@ -46,7 +102,7 @@ public class Main {
         String strNumColuna = linha.substring(linha.indexOf(" ")).trim();
         int numColuna = Integer.parseInt(strNumColuna);
         
-        ag = new AlgoritmoGenetico(numLinha, numColuna, TAM_POPULACAO, TAXA_MIN_MUTACAO, NUM_GERACOES);
+        algoritmoGenetico = new AlgoritmoGenetico(numLinha, numColuna, TAM_POPULACAO, TAXA_MIN_MUTACAO, NUM_GERACOES);
         
         br.readLine();
         
@@ -55,12 +111,19 @@ public class Main {
             String dados[] = linha.split("\\s+");
             
             double custo = Double.parseDouble(dados[2]);
-            ag.addCusto(i, custo);
+            algoritmoGenetico.addCusto(i, custo);
            
             for (int j = 3; j < dados.length; j++) {
-                ag.addDados(i, Integer.parseInt(dados[j]));
+                algoritmoGenetico.addDados(i, Integer.parseInt(dados[j]));
             }
         }
-        
+
+        algoritmoGenetico.executar();
+
+        long finish = System.nanoTime(); //tempo final
+        long time = (finish - start);
+        long time_ms = TimeUnit.NANOSECONDS.toMillis(time);
+
+        System.out.println("\nTempo de execução (ms): "+ time_ms);
     }
 }
